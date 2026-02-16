f83ac3587d28a754328fed56647080bb0970a5c7
Fáber Sándor <sandorwow@gmail.com>
2026-02-16 20:03:34 +0100

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,13 @@
-// Bioacoustic_Visualizer/build.gradle.kts
-
 buildscript {
+    repositories {
+        google()
+        mavenCentral()
+    }
     dependencies {
         classpath("com.android.tools.build:gradle:8.2.2")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.22")
     }
 }
 
-// Itt NE legyen repositories blokk, mert azt a settings.gradle.kts intézi!
+// Ebben a fájlban NINCS allprojects és NINCS repositories blokk legalul!
+
