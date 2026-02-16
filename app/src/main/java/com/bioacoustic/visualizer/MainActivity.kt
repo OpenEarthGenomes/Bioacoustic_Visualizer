cdee531c8cc4da89b774bb69f6a35c921a662a0c
Fáber Sándor <sandorwow@gmail.com>
2026-02-16 16:29:21 +0100

    Create MainActivity.kt

---
+++ app/src/main/java/com/bioacoustic/visualizer/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.bioacoustic.visualizer
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            Text("BioVisualizer v1.0 - Android 16")
+        }
+    }
+}
