65c35f9dc166cc26892b82881f324582d7b1eac6
Fáber Sándor <sandorwow@gmail.com>
2026-02-17 20:23:57 +0100

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,17 +4,15 @@ plugins {
 }
 
 android {
-    // Ez a namespace kapcsolja össze a kódot a Gradle-lel
     namespace = "com.bioacoustic.visualizer"
-    compileSdk = 35
+    compileSdk = 35 // Android 16-hoz már a legmagasabb kell
 
     defaultConfig {
         applicationId = "com.bioacoustic.visualizer"
-        // Android 8.0-tól felfelé mindenhol futni fog
         minSdk = 26
         targetSdk = 35
         versionCode = 1
-        versionName = "1.0-BioAcoustic"
+        versionName = "1.0-A16-FIX"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
@@ -22,10 +20,14 @@ android {
     buildTypes {
         release {
             isMinifyEnabled = false
-            proguardFiles(
-                getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro"
-            )
+            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+        }
+    }
+
+    // ANDROID 16 SPECIÁLIS BEÁLLÍTÁS: 16KB-os lapméret támogatása
+    packaging {
+        jniLibs {
+            useLegacyPackaging = true // Segít a betöltésben régebbi és új rendszereken is
         }
     }
 
@@ -37,36 +39,22 @@ android {
     kotlinOptions {
         jvmTarget = "17"
     }
-
-    // EZ A RÉSZ JAVÍTJA A "databinding/viewbinding" HIBÁKAT
-    buildFeatures {
-        viewBinding = true
-        dataBinding = true
-    }
-
-    // A Filament 3D motor fájljait nem szabad tömöríteni
-    androidResources {
-        noCompress += listOf("filamat", "glb", "gltf")
-    }
 }
 
 dependencies {
-    // Alapvető Android könyvtárak
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.11.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
-    
-    // 3D MEGJELENÍTÉS (Filament 1.45.0)
-    implementation("com.google.android.filament:filament-android:1.45.0")
-    implementation("com.google.android.filament:filament-utils-android:1.45.0")
-    
-    // HANGFELDOLGOZÁS (FFT és Coroutines)
-    implementation("com.github.wendykierp:JTransforms:3.1")
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.3")
 
-    // Teszteléshez
-    testImplementation("junit:junit:4.13.2")
-    androidTestImplementation("androidx.test.ext:junit:1.1.5")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    // FILAMENT - A legfrissebb verzió kell az Android 16 kompatibilitáshoz
+    implementation("com.google.android.filament:filament-android:1.51.0")
+    implementation("com.google.android.filament:gltfio-android:1.51.0")
+    implementation("com.google.android.filament:filament-utils-android:1.51.0")
+
+    // MATEK (FFT)
+    implementation("com.github.wendykierp:JTransforms:3.1")
+
+    // KORUTINOK
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.3")
 }
