7702bdf421f98928c2eea6b1b902b6d2b823fbf2
Fáber Sándor <sandorwow@gmail.com>
2026-02-17 21:01:23 +0100

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,29 +5,20 @@ plugins {
 
 android {
     namespace = "com.bioacoustic.visualizer"
-    compileSdk = 35 // Android 16-hoz már a legmagasabb kell
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.bioacoustic.visualizer"
         minSdk = 26
         targetSdk = 35
         versionCode = 1
-        versionName = "1.0-A16-FIX"
-
-        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        versionName = "1.0"
     }
 
-    buildTypes {
-        release {
-            isMinifyEnabled = false
-            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
-        }
-    }
-
-    // ANDROID 16 SPECIÁLIS BEÁLLÍTÁS: 16KB-os lapméret támogatása
+    // Ez kényszeríti ki a C++ motor helyes betöltését
     packaging {
         jniLibs {
-            useLegacyPackaging = true // Segít a betöltésben régebbi és új rendszereken is
+            useLegacyPackaging = true
         }
     }
 
@@ -35,26 +26,19 @@ android {
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
     }
-
-    kotlinOptions {
-        jvmTarget = "17"
-    }
 }
 
 dependencies {
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.11.0")
-    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
-
-    // FILAMENT - A legfrissebb verzió kell az Android 16 kompatibilitáshoz
-    implementation("com.google.android.filament:filament-android:1.51.0")
-    implementation("com.google.android.filament:gltfio-android:1.51.0")
-    implementation("com.google.android.filament:filament-utils-android:1.51.0")
-
-    // MATEK (FFT)
+    
+    // Stabilabb Filament verzió Samsung A35-höz
+    implementation("com.google.android.filament:filament-android:1.48.0")
+    implementation("com.google.android.filament:gltfio-android:1.48.0")
+    implementation("com.google.android.filament:filament-utils-android:1.48.0")
+    
     implementation("com.github.wendykierp:JTransforms:3.1")
-
-    // KORUTINOK
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.3")
 }
+
