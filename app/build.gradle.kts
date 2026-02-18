25224afc36722614b4dce0c7f69cf82dd7b5c89a
Fáber Sándor <sandorwow@gmail.com>
2026-02-18 08:58:28 +0100

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,15 +13,6 @@ android {
         targetSdk = 35
         versionCode = 1
         versionName = "1.0"
-
-        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-    }
-
-    buildTypes {
-        release {
-            isMinifyEnabled = false
-            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
-        }
     }
 
     packaging {
@@ -35,8 +26,9 @@ android {
         targetCompatibility = JavaVersion.VERSION_17
     }
 
-    kotlinOptions {
-        jvmTarget = "17"
+    // DeepSeek javaslata alapján a legújabb formátumra javítva:
+    kotlin {
+        jvmToolchain(17)
     }
 }
 
@@ -44,16 +36,12 @@ dependencies {
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.11.0")
-    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
-
-    // Filament - A verzió 1.51.0-ra javítva a build hiba miatt
+    
+    // Filament 1.51.0 - Ez kell a build sikeréhez
     implementation("com.google.android.filament:filament-android:1.51.0")
     implementation("com.google.android.filament:gltfio-android:1.51.0")
     implementation("com.google.android.filament:filament-utils-android:1.51.0")
-
-    // FFT matek
+    
     implementation("com.github.wendykierp:JTransforms:3.1")
-
-    // Korutinok
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.3")
 }
